54b6ce97118789182c1dbb1054a92159d7453225
Diego Curti <diegocurti@hotmail.com>
2025-06-10 19:02:34 +0200

    fix: capacitor dependencies
    "@capacitor/storage" is deprecated.
    Now Capacitor uses "@capacitor/preferences".
    
    Note: this is not tested, it just compiling ok

--- src/services/storage-service.ts
+++ src/services/storage-service.ts
@@ -1,16 +1,16 @@
 
-import { Storage } from '@capacitor/storage';
+import { Preferences } from '@capacitor/preferences';
 
 export class StorageService {
   static async set(key: string, value: any): Promise<void> {
-    await Storage.set({
+    await Preferences.set({
       key,
       value: JSON.stringify(value),
     });
   }
 
   static async get<T>(key: string): Promise<T | null> {
-    const item = await Storage.get({ key });
+    const item = await Preferences.get({ key });
     if (item.value) {
       return JSON.parse(item.value) as T;
     }
@@ -18,15 +18,15 @@ export class StorageService {
   }
 
   static async remove(key: string): Promise<void> {
-    await Storage.remove({ key });
+    await Preferences.remove({ key });
   }
 
   static async clear(): Promise<void> {
-    await Storage.clear();
+    await Preferences.clear();
   }
 
   static async keys(): Promise<string[]> {
-    const { keys } = await Storage.keys();
+    const { keys } = await Preferences.keys();
     return keys;
   }
 }
